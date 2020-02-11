7917835e3ee59a59ab04f935097f9c0e483374ce
OOZYGRUB <OozyGrub@hotmail.com>
2020-02-11 16:21:27 +0700

    Updates

---
+++ precache-manifest.72a898a44f9affc254ac2700ce06a1fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17231917aa5f1502459a7a6fb2c0cf3d",
+    "url": "/oufeow-brief-history/index.html"
+  },
+  {
+    "revision": "13c38d83980e97955188",
+    "url": "/oufeow-brief-history/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2bea56a16c7f63086e8b",
+    "url": "/oufeow-brief-history/static/js/2.379488c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/oufeow-brief-history/static/js/2.379488c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c38d83980e97955188",
+    "url": "/oufeow-brief-history/static/js/main.88bcc40f.chunk.js"
+  },
+  {
+    "revision": "58e8233ee485d6b344bf",
+    "url": "/oufeow-brief-history/static/js/runtime-main.11a8f7fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oufeow-brief-history/static/media/logo.5d5d9eef.svg"
+  }
+]);
