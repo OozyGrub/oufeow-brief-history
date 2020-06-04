326f090a8b01da39ebae2267e77e5c56682f4c53
OOZYGRUB <OozyGrub@hotmail.com>
2020-06-04 19:58:45 +0700

    add StoryPage

---
+++ src/StoryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function StoryPage() {
+  return (
+      <div>
+        <button>Story Page!</button>
+      </div>
+  );
+}
+
+export default StoryPage;
