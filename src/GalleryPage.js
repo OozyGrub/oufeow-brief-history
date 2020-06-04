cacd3a9e6fff5957a3a8d9d27e4106803f8b364e
OOZYGRUB <OozyGrub@hotmail.com>
2020-06-04 19:59:14 +0700

    add GalleryPage

---
+++ src/GalleryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function GalleryPage() {
+  return (
+      <div>
+        <button>Hello World!</button>
+      </div>
+  );
+}
+
+export default GalleryPage;
