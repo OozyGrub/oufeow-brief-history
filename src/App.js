6c4c4f0b09f39223a26995dba2ba60e6c0a0aa75
OOZYGRUB <OozyGrub@hotmail.com>
2020-06-05 11:04:02 +0700

    import CovidPage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import HomePage from "./HomePage";
 import GalleryPage from "./GalleryPage";
 import NavBar from "./NavBar";
 import StoryPage from "./StoryPage";
+import CovidPage from "./CovidPage";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Route exact path="/"><HomePage /></Route>
         <Route exact path="/gallery"><GalleryPage /></Route>
         <Route exact path="/story"><StoryPage /></Route>
+        <Route exact path="/covid"><CovidPage /></Route>
       </Switch>
     </Router>
   );
