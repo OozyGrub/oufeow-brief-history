8f92457dbbcf5a6e24114a491c488a82f4ef02e6
OOZYGRUB <OozyGrub@hotmail.com>
2020-06-05 14:16:38 +0700

    ➕ CovidCard Component

---
+++ src/CovidCard.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import styled from 'styled-components'
+
+const Color = ["black","purple"];
+const Card = styled.div`
+  background: ${props => Color[props.color]};
+  color: white;
+  border-radius: 7px;
+  padding: 20px;
+  margin: 10px;
+  font-size: 16px;
+
+`;
+
+function CovidCard(props) {
+    return (
+      <React.Fragment>
+      {
+        !props.isLoading ? (
+          <Card color="0">
+            <p>{props.covidTitle}</p>
+            <p>{props.covidData}</p>
+            <button onClick={props.handleChange}>
+              Add
+            </button>
+          </Card>
+        ) : (
+          <div style={{color: "red"}}>
+            <p>Loading...</p>
+          </div>
+        )
+      }
+      </React.Fragment>
+      
+    );
+}
+
+export default CovidCard;
